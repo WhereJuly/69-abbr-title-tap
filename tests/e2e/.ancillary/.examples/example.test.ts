4eec2742e59ac6236a7ee23d6a5def2a0c1c1dc4
WhereJuly <where.july@googlemail.com>
2025-07-04 16:41:56 +0300

    Update (icicdr): test environment setup

--- tests/e2e/.ancillary/.examples/example.test.ts
+++ tests/e2e/.ancillary/.examples/example.test.ts
@@ -1,10 +1,14 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Playwright page', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  test('Should have title', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
 });
 
 test('get started link', async ({ page }) => {
--- tests/e2e/.ancillary/config/playwright.config.ts
+++ tests/e2e/.ancillary/config/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-        ['list'],
+        ['list', { printSteps: true }],
         // ['html', { outputFolder: `${testRoot}/.ancillary/.reports/html` }] // NB: Example
     ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
