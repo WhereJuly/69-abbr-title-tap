b66e7476181a6cd072317b57d7cc287f308b4ce8
WhereJuly <where.july@googlemail.com>
2025-07-07 18:45:19 +0300

    Construct (core): abbr tap handler
    - isolate for further tdd

---
+++ src/ts/AbbrTapHandler.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+import { ATapHandler } from '@src/ts/ATapHandler.js';
+import { ATT_CLASS_ON, ATT_VARIABLE_TITLE_LEFT, ATT_VARIABLE_TITLE_TOP } from '@src/ts/style.tokens.js';
+
+type TTitleCoords = { top: number; left: number; };
+
+export default class AbbrTapHandler extends ATapHandler {
+
+    constructor() {
+        super();
+    }
+
+    public handle(el: HTMLElement): void {
+        this.el = el;
+        this.cleanUp(document.querySelectorAll('abbr'));
+
+        // NB: Should not process the non-abbr elements
+        if (!this.isAbbrTag()) { return; }
+
+        this.setTitleCoords(this.el);
+
+        this.el.classList.add(ATT_CLASS_ON);
+    }
+
+    private cleanUp(abbrEls: NodeListOf<HTMLElement>): void {
+        abbrEls.forEach((abbrEl: HTMLElement) => {
+            abbrEl.classList.remove(ATT_CLASS_ON);
+
+            abbrEl.style.removeProperty(ATT_VARIABLE_TITLE_TOP);
+            abbrEl.style.removeProperty(ATT_VARIABLE_TITLE_LEFT);
+        });
+    }
+
+    // NB: Must be called only after setting `el` on `this`.
+    private isAbbrTag(): boolean {
+        return this.el?.tagName.toLowerCase() === 'abbr';
+    }
+
+    private setTitleCoords(abbrEl: HTMLElement): void {
+        // NB: Calculate the top-left coordinates of the abbr:after element.
+        const coords = this.titleCoords(abbrEl);
+
+        abbrEl.style.setProperty(ATT_VARIABLE_TITLE_TOP, `${coords.top}px`);
+        abbrEl.style.setProperty(ATT_VARIABLE_TITLE_LEFT, `${coords.left}px`);
+    }
+
+    private titleCoords(abbrEl: HTMLElement): TTitleCoords {
+        const round = (value: number) => Math.round(value);
+        const rect = abbrEl.getBoundingClientRect();
+
+        const offset = { top: rect.height / 3, left: 10 };
+        return { top: round(rect.bottom + offset.top), left: round(rect.x + offset.left) };
+    }
+
+}
---
+++ tests/e2e/_tests/integration/AbbrTapHandler.test.ts
@@ -0,0 +1,71 @@
+'use strict';
+
+import { test, expect, TestInfo, PlaywrightTestArgs } from '@playwright/test';
+import { ATT_CLASS_ON } from '@src/ts/style.tokens.js';
+
+import { hasVisibleAfter, htmlContent } from '@tests/e2e/.ancillary/helpers/helpers.js';
+
+test.describe('AbbrTapHandler Test', () => {
+
+    test.beforeEach(async ({ page }: PlaywrightTestArgs, testInfo: TestInfo) => {
+        // Set up basic page DOM
+        const html = htmlContent('page.html', testInfo);
+        await page.setContent(html, { waitUntil: 'load' });
+
+        // WARNING: Run `npm run build:test` after update and before using it here 
+        await page.addScriptTag({ path: '.delivery/.builds/test/initialization.js', type: 'module' });
+        await page.addStyleTag({ path: '.delivery/.builds/test/styles.css' });
+
+        // Forward all console messages from page to Node.js console
+        page.on('console', msg => { console.log(`PAGE LOG [${msg.type()}]: ${msg.text()}`); });
+    });
+
+    test('Should successfully tap on first <abbr>', async ({ page }: PlaywrightTestArgs) => {
+        expect(page).toBeTruthy();
+
+        const abbr = page.locator('abbr').first();
+        await abbr.waitFor();
+
+        // Act
+        await abbr?.dispatchEvent('touchstart');
+        await abbr?.dispatchEvent('touchend');
+
+        // Assert
+        const class1 = await abbr.getAttribute('class');
+        expect(class1).toEqual(ATT_CLASS_ON);
+    });
+
+    test('Should remove classes from the first tapped <abbr> when tap on second <abbr>', async ({ page }: PlaywrightTestArgs) => {
+        expect(page).toBeTruthy();
+
+        await page.pause();
+
+        const abbr1 = page.locator('abbr').first();
+        await abbr1.waitFor();
+
+        // Arrange
+        await abbr1?.dispatchEvent('touchstart');
+        await abbr1?.dispatchEvent('touchend');
+
+        const abbr2 = page.locator('abbr').last();
+        await abbr2.waitFor();
+
+        // Act
+        await abbr2?.dispatchEvent('touchstart');
+        await abbr2?.dispatchEvent('touchend');
+
+        const class1 = await abbr1.getAttribute('class');
+        const class2 = await abbr2.getAttribute('class');
+
+        expect(class1).toEqual('');
+        expect(class2).toEqual(ATT_CLASS_ON);
+
+        const abbr1HasVisibleAfter = await hasVisibleAfter(abbr1);
+        const abbr2HasVisibleAfter = await hasVisibleAfter(abbr2);
+
+        expect(abbr1HasVisibleAfter).toEqual(false);
+        expect(abbr2HasVisibleAfter).toEqual(true);
+    });
+
+});
+
