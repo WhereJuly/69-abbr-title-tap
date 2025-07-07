52eb836b80ae5cc3985fcd03d854f649b3091a24
WhereJuly <where.july@googlemail.com>
2025-07-07 13:15:29 +0300

    Update (icicdr): test environment setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ export default tseslint.config(eslint.configs.recommended, tseslint.configs.reco
         }
     },
     rules: {
+        '@typescript-eslint/no-floating-promises': 'warn',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-namespace': 'off',
         '@typescript-eslint/unbound-method': 'off',
