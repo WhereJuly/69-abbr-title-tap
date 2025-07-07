a47fae3f93ae8cf2847b5f4add463f7dad46a309
WhereJuly <where.july@googlemail.com>
2025-07-07 17:00:50 +0300

    Construct (core): generate LESS variables names

---
+++ src/ts/style.tokens.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * WARNING: These constants, along with TS, are used in the LESS CSS for auto generating constants
+ * via .delivery\scripts\generate-style-constants.ts.
+ * 
+ * @see {@link src\styles\styles.less}
+ * @see {@link src\styles\readonly\_constants.less}
+ * @see {@link .delivery\scripts\generate-style-constants.ts}
+ */
+export const ATT_CLASS_ON = 'att-on';
+
+export const ATT_VARIABLE_TITLE_TOP = '--title-top';
+export const ATT_VARIABLE_TITLE_LEFT = '--title-left';
